359d6e68e883bb143abe9dfa197f859d63ab84f9
yinghuan <ec.huyinghuan@gmail.com>
2021-11-11 15:55:15 +0800

    template

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import { useAppDispatch, useAppSelector } from '@src/hook'
+import { addCount, addCountAsync } from "@store/demo-slice"
+
+const IndexPages: FC = () => {
+    const count = useAppSelector((state) => { return state.Demo.count })
+    const dispatch = useAppDispatch()
+    return (
+        <div>
+            <p><label>数量:</label>{count}</p>
+            <p>
+                <button type="button" onClick={() => { dispatch(addCount(1)) }}>+1</button>
+                <button type="button" onClick={() => { dispatch(addCount(-1)) }}>-1</button>
+            </p>
+            <p>
+                <button type="button" onClick={() => { dispatch(addCountAsync({ count: 1 })) }}>延时+1</button>
+            </p>
+        </div>
+    )
+}
+
+export default IndexPages
